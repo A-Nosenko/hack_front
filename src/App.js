f81c128be1d57ef1f683ed543b5f8ee3051b2d1f
nosenko.a <nosenko.a@dbbest.com>
2020-11-22 13:45:24 +0200

    [*/-] Added buttons for login, logout, register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userType: this.NO_USER
+    };
+  }
+
+  SIMPLE_USER = () => "simple_user";
+  TRAINER = () => "trainer";
+  ORGANISER = () => "organiser";
+  NO_USER = () => "no_user";
+
+  simpleUserRegister = () => {
+    console.log("\t\t\t>>> simpleUserRegister");
+    // Появляется форма регистрации для simpleUser
+    this.setState({userType: this.SIMPLE_USER})
+  };
+
+  trainerRegister = () => {
+    console.log("\t\t\t>>> trainerRegister");
+    // Появляется форма регистрации для trainer
+    this.setState({userType: this.TRAINER})
+  };
+
+  organiserRegister = () => {
+    console.log("\t\t\t>>> organiserRegister");
+    // Появляется форма регистрации для organiser
+    this.setState({userType: this.ORGANISER})
+  };
+
+  enter = () => {
+    console.log("\t\t\t>>> enter");
+    // Появляется форма логина
+  };
+
+  out = () => {
+    console.log("\t\t\t>>> out");
+    this.setState({userType: this.NO_USER})
+  };
+
+  getRegisterButtons = (userType) => {
+    if (userType === this.NO_USER) {
+      return <div className="Register-buttons">
+        <button onClick={this.simpleUserRegister}>Зарегистрироваться как обычный пользователь</button>
+        <button onClick={this.trainerRegister}>Зарегистрироваться как тренер</button>
+        <button onClick={this.organiserRegister}>Зарегистрироваться как организатор</button>
+      </div>
+    } else {
+      return <div/>;
+    }
+  };
+
+  getCabinet = (userType) => {
+    switch (userType) {
+      case this.SIMPLE_USER:
+        return <div>Кабинет обычного пользователя</div>;
+      case this.ORGANISER:
+        return <div>Кабинет организатора</div>;
+      case this.TRAINER:
+        return <div>Кабинет тренера</div>;
+      default:
+        return <div/>;
+    }
+  };
+
+  render() {
+    console.log("\t\t\t>>> RENDER");
+    console.log("\t\t\t>>> USER TYPE: " + this.state.userType);
+    return (
+        <div>
+          <div className="App-header">
+            {this.state.userType === this.NO_USER ? <button onClick={this.enter}>Вход</button> :
+                <button onClick={this.out}>Выход</button>}
+          </div>
+
+          <div className="App-body">
+            {this.getRegisterButtons(this.state.userType)}
+            {this.getCabinet(this.state.userType)}
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
